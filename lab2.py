c0d21159d89661d43ceb2f456da32277bf0dfeda
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-09-21 16:36:57 +0600

    second commit

---
+++ lab2.py
@@ -0,0 +1,15 @@
+#Abylkassym Aruzhan
+#laboratory work 2
+#Here we will compare 3 numbers and dfind minimum in 3 of them
+print("enter 1st number: ")
+a = int(input()) 
+print("enter 2nd number: ")
+b = int(input())
+print("enter 3rd number: ")
+c = int(input())
+if b >= a <= c:
+    print(a+"this number is the minimum")
+elif a >= b <= c:
+    print(b+"this number is the minimum")
+else:
+    print(c+"this number is the minimum")
