595a79d272a4b90d209cdf86626dddba53016c6b
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-09-28 16:44:06 +0600

    apai

---
+++ lab3.11.py
@@ -0,0 +1,12 @@
+#number = int(input())
+#number1 = int(input())
+#sum=0;
+#for i in range (number , number1+1):
+ # print(i)
+
+#2
+number = int(input())
+number1 = int(input())
+sum=0;
+for i in range (number , number1+1 ):
+  print(i)
