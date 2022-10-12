ac6625f36104567d0caa724c4df4518bc73d28bd
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-10-12 16:13:50 +0600

    lab5

---
+++ lab5_1.py
@@ -0,0 +1,80 @@
+#lab 5.1
+last_names = []
+n=int(input())
+grades = []
+while n>0:
+  last_names.append(input("name: "))
+  grades.append(input("course: "))
+  n=n-1
+
+print(last_names[0]+": "+grades[0]+"\n"+last_names[1]+": "+grades[1]+"\n"+last_names[2]+": "+grades[2]+"\n")
+print(grades.sort())
+
+
+#lab5.2
+# a = [["RM", 100],
+#      ["Jin", 90], ["Suga",99]]
+# name = input("name: ")
+# for i in range(len(a)):
+#     for j in range(len(a)):
+#         if name == a[i][0]:
+#             print(a[i][0], ": ", a[i][1])
+            
+#         else:
+#             print("error")
+
+
+#lab5.3
+
+            
+
+#lab5.3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a = [["Zhanerke", 80],
+#      ["Aruzhan", 85]]
+# name = input("name:")
+# for i in range(len(a)):
+#     for j in range(len(a)):
+#         if name == a[i][0]:
+#             print(a[i][0], ":", a[i][1])
+#             break
+#         else:
+#             print("Ondai student joq!")
+#             break
+
+
+  
+
+
+# print(n)
+# grades = []
+# i=0
+# while last_names.==0:
+#   print(last_names[i] , ": ",grades.append(input()))
+#   i=i+1
+#   n=n-1
+# print(last_names,"\t",grades)
+
+
+
+
+
+
+#lab5.2
+
