e25663dfff541eea23ea5ea9d21478e7dc4b1f02
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-10-19 16:22:51 +0600

    last67

--- lab6_7.py
+++ lab6_7.py
@@ -82,3 +82,17 @@ print(dict)
 
 
 #3. Берілген есептерден бір есепті таңдап шығару
+#  Словарь синонимов
+# Вам дан словарь, состоящий из пар слов. 
+# Каждое слово является синонимом к парному ему слову. 
+# Все слова в словаре различны. Для одного данного слова определите его синоним.
+
+
+dictionary = {}
+n = int(input())
+for i in range(n):
+    first, second = input().split()
+    dictionary[first] = second
+    dictionary[second] = first
+
+print(dictionary[input()])
