2d953d0533f28e244e0da75967506b1fba37a91d
Aruzhan Abylkassym <forcookiearu@gmail.com>
2022-11-23 16:09:39 +0600

    lab 10 added

--- lab10/lab10.py
+++ lab10/lab10.py
@@ -1,7 +1,37 @@
-# 10 кірістірілген функцияны алып, бір бірімен үйлестіріп бағдарлама жазамыз. 
-# Функциялар осыған дейін біз қолданбаған болуы қажет.  
+file = open('lab10.txt', 'w')
+
+while True:
+    i = input("Введите элемент: ")
+    if i ==" " or i =="":
+        break
+    file.writelines(str(i) + '\n')
+file.close()
+
+lst = []
+file1 = open('lab10.txt', 'r')
+
+while True:
+    line = file1.readline()
+    if not line:
+        break
+    lst.append(int(line.strip()))
+print(lst)
 
 
+print("SORTED LIST",sorted(lst))
+print("MIN ELEMENT OF LIST: ", min(lst))
+print("MAX ELEMENT OF LIST: ", max(lst))
+print("LIST IN BIN ",bin(lst[1]))
+print("LIST IN HEX",hex(lst[1]))
+print("LIST IN OCT", (lst[1]))
 
-x = ["  Яблоко", "Апельсин", "Гранат"]
-print(dir(x))
+print("NUMERATED LIST: ",list(enumerate(lst)))
+print("NUMBER IN ASCII CODE ", chr(lst[1]))
+print("LIST IN BYTES ",bytes(lst))
+print(reversed(lst))
+
+file1.close()
+
+lst2 = [2, 1, 3, 4, 7]
+for i, n in zip(lst, lst2):      
+    print(n, i)
